3dd3932cfa6d578629b54cf31977fdd604ba0b9c
veleboks <skobelev.exe@gmail.com>
2022-09-10 15:50:34 +0300

    Make structure of files

--- train.py
+++ train.py
@@ -2,19 +2,23 @@ import argparse
 import N_gram
 import re
 
-with open('belkin.txt') as f:
+def clean_text(raw):
+    clean = raw.lower().replace('ё', 'e')
+    clean = re.sub("(?!(?<=\w)[-']\w)\W", ' ', clean, flags = re.U)
+    clean = ' '.join(clean.split())
+    return clean
+
+with open('data/belkin.txt') as f:
     raw = f.read()
 
-raw = raw.lower().replace('ё', 'e')
-clean = re.sub("(?!(?<=\w)[-']\w)\W", ' ', raw, flags = re.U)
-clean = ' '.join(clean.split())
+clean = clean_text(raw)
 
-with open('clean_data.txt', 'w') as f:
-    f.write(clean)
+# with open('clean_data.txt', 'w') as f:
+    # f.write(clean)
 
 data = [clean.split()]
 
-model = N_gram.N_gram(2)
+model = N_gram.N_gram()
 
 model.fit(data)
 
